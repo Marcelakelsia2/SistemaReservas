7dea27631e550c43f7bd1d11c41a3c9d97e29682
Panzo Rafael Chiló <panzo1972@gmail.com>
2026-07-06 15:05:54 +0100

    Remove env_file do docker-compose para corrigir deploy

--- backendkelsia/start.sh
+++ backendkelsia/start.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+echo "=== Variáveis de ambiente ==="
+
+echo "DATABASE_URL=$DATABASE_URL"
+echo "JWT_SECRET=$JWT_SECRET"
+echo "GOOGLE_CLIENT_ID=$GOOGLE_CLIENT_ID"
+echo "PORT=$PORT"
+
+echo "============================="
+
 echo "A aplicar migrações..."
 npx prisma migrate deploy
 
