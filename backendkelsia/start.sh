0b4e4bcc6b1a0937c5acae3357206a02e82e6739
Panzo Rafael Chiló <panzo1972@gmail.com>
2026-07-06 15:20:34 +0100

    atualizacao

--- backendkelsia/start.sh
+++ backendkelsia/start.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
 echo "=== Variáveis de ambiente ==="
-
 echo "DATABASE_URL=$DATABASE_URL"
 echo "JWT_SECRET=$JWT_SECRET"
 echo "GOOGLE_CLIENT_ID=$GOOGLE_CLIENT_ID"
 echo "PORT=$PORT"
-
 echo "============================="
+
+# --- AGUARDAR PELO POSTGRES ---
+echo "A aguardar que a base de dados (postgres:5432) esteja pronta..."
+while ! nc -z postgres 5432; do
+  sleep 1
+done
+echo "Base de dados ativa e online!"
+# ------------------------------
 
 echo "A aplicar migrações..."
 npx prisma migrate deploy
